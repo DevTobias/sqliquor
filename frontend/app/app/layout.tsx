9a4057b73d48af9e50b7e7a314121ee7df8fa7ea
Tobias Kärst <tobi.kaerst@gmx.de>
2023-06-23 11:10:02 +0000

    feat: added brand assets

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,6 +1,7 @@
 import '$lib/styles/base/foundation.scss';
+import { Metadata } from 'next';
 
-export const metadata = { title: 'SQL Game Demo' };
+export const metadata: Metadata = { title: 'SQLiquor', icons: { icon: 'favicon.ico' } };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
